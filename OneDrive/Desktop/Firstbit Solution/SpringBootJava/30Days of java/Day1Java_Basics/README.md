af8656b82c4dece3704c3bdb92bb2b1368b4b070
Shubham Gaikwad <shubham41shubham@gmail.com>
2025-07-13 17:23:08 +0530

    ✅ Re-added Day1Java_Basics with all files

---
+++ OneDrive/Desktop/Firstbit Solution/SpringBootJava/30Days of java/Day1Java_Basics/README.md
@@ -0,0 +1,12 @@
+# Day 01 – Java Basics
+
+## Topics:
+- Java Architecture
+- Variables, Scanner
+- If-else logic
+
+## Output:
+Enter first number: 5  
+Enter second number: 10  
+Enter operator: +  
+Result = 15
---
+++ OneDrive/Desktop/Firstbit Solution/SpringBootJava/30Days of java/Day1Java_Basics/src/com/calculater/Calculator.java
@@ -0,0 +1,40 @@
+package com.calculater;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter first number: ");
+        int a = sc.nextInt();
+        
+        System.out.print("Enter second number: ");
+        int b = sc.nextInt();
+        
+        System.out.print("Enter operator (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        int result;
+
+        if (op == '+') {
+            result = a + b;
+        } else if (op == '-') {
+            result = a - b;
+        } else if (op == '*') {
+            result = a * b;
+        } else if (op == '/') {
+            result = a / b;
+        } else {
+            System.out.println("Invalid operator!");
+            return;
+        }
+
+        System.out.println("Result = " + result);
+
+	}
+
+}
